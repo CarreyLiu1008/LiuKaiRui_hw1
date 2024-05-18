ff759b8b51fbace6f2b88cf2249d1cfff7a6a360
CarreyLiu <3230102307@zju.edu.cn>
2024-05-18 21:47:16 +0800

    LiuKaiRui

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "C:/code/VSC/Xlab/algebra/bin/libmain.dll.a"
+  "C:/code/VSC/Xlab/algebra/bin/main.exe"
+  "C:/code/VSC/Xlab/algebra/bin/main.exe.manifest"
+  "C:/code/VSC/Xlab/algebra/bin/main.pdb"
+  "CMakeFiles/main.dir/src/algebra.c.obj"
+  "CMakeFiles/main.dir/src/algebra.c.obj.d"
+  "CMakeFiles/main.dir/src/main.c.obj"
+  "CMakeFiles/main.dir/src/main.c.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
